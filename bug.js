70f712accb2d12d2cd7aaa54462a8ccf2c3b5c76
BugHunter-X <askzenai@gmail.com>
2024-12-06 23:56:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+<div class="flex items-center justify-center h-screen">
+  <div class="bg-gray-800 p-8 rounded-lg shadow-md">
+    <h1 class="text-3xl font-bold text-white mb-4">Error: The background color is not applied</h1>
+    <p class="text-gray-300">This is because you may have added another class or there is an error in the structure </p>
+  </div>
+</div>
+```
